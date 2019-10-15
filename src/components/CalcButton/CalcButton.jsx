d2417b274fff5bbfa8813042c79aa212d935fee8
Josue Valenzuela <josue@kemik.gt>
2019-10-15 14:06:33 -0600

    Add tests

--- src/components/CalcButton/CalcButton.jsx
+++ src/components/CalcButton/CalcButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './CalcButton.css';
+//import './CalcButton.css';
 
 const CalcButton = (props) => {
   const { label, type, disabled } = props;
